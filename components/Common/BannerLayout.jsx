d4ca0ebe5bc7d8f68e50ba81c3e81a753211251b
hoangle217 <hoangxam2@gmail.com>
2024-11-22 06:58:08 +0700

    fix backgroud size

--- components/Common/BannerLayout.jsx
+++ components/Common/BannerLayout.jsx
@@ -6,6 +6,7 @@ const BannerLayout = ({ children }) => {
             style={{
                 background: 'url(images/background.png)',
                 backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
             }}>
             <div className="absolute inset-0 z-20 flex flex-col items-center justify-center w-full h-full bg-gradient-to-t from-MidNightBlack">
                 <div className="bg-Black/5 backdrop-blur-sm w-full h-full">
